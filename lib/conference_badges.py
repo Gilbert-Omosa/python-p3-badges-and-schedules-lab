8fdb8f68824957c0d19d3227aa6aa6ef34995ae2
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-12-07 09:33:48 +0300

    Done with Assignment

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,18 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
-def batch_badge_creator(names):
-    return None
+def batch_badge_creator(speakers):
+    return [badge_maker(name) for name in speakers]
 
-def assign_rooms(names):
-    return None
+def assign_rooms(speakers):
+    return [f"Hello, {name}! You'll be assigned to room {i}!" for i, name in enumerate(speakers, 1)]
 
-def printer(names):
-    return None
+def printer(speakers):
+    badges = batch_badge_creator(speakers)
+    messages = assign_rooms(speakers)
+
+    for badge in badges:
+        print(badge)
+
+    for message in messages:
+        print(message)
